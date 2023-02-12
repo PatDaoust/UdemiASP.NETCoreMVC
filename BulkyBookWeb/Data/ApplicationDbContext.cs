cd97a66184e38d1303a5008f6ee79835c8f9f132
Cataluna <catalunalilith2680@gmail.com>
2023-02-11 21:01:23 -0500

    Implement section 2

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) {
+
+        }
+
+        //creates category table names "Categories" with the 4 columns from the class Category's properties
+        //is code first appreach
+        public DbSet<Category> Categories { get; set; }
+    } 
+}
